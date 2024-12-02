8c7073551f288317486ae29bd029f1a95be78b95
hrdl <git@hrdl.eu>
2024-12-02 10:55:36 +0100

    Move menu logic into toggle_menu.sh, add to button to waybar

--- misc/bin/launch_lisgd.sh
+++ misc/bin/launch_lisgd.sh
@@ -9,4 +9,4 @@ lisgd -d /dev/input/by-path/platform-fe5e0000.i2c-event \
     -g "3,RL,*,*,R,sway_workspace goto next &" \
     -g "4,LR,*,*,R,sway_workspace move prev &" \
     -g "4,RL,*,*,R,sway_workspace move next &" \
-    -g "4,UD,*,*,R,nwg-menu -fm pcmanfm -term kitty -va top -isl 64 -iss 64 &" &
+    -g "4,UD,*,*,R,toggle_menu.sh &" &
